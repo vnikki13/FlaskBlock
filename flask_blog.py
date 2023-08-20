3726a547774f929957c3d3cb490eb1fd7bde2ccd
Nikki Vaughan <vnikki13@gmail.com>
2023-08-20 12:50:55 -0400

    Add Bootstrap styling

--- flask_blog.py
+++ flask_blog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Nikki Vaughan',
+        'title': 'Blog Post 1',
+        'content': 'First blog post',
+        'date_posted': 'Aug 19, 2023'
+    },
+    {
+        'author': 'Nikki Vaughan',
+        'title': 'Blog Post 2',
+        'content': 'Second blog post',
+        'date_posted': 'Aug 20, 2023'
+    }
+]
+    
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page!</h1>"
+    return render_template("home.html", posts=posts, title="About")
 
 @app.route("/about")
 def about():
-    return "<h1>About Page!</h1>"
+    return render_template("about.html")
 
 app.run(
     host="0.0.0.0",
